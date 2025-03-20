9158b4ef36cc94e4f8b13faa2ccfe6d3d1db06b5
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-03-20 18:06:45 +0530

    added fix for create employee webook

--- server/routes/webHookRoutes.js
+++ server/routes/webHookRoutes.js
@@ -11,7 +11,6 @@ router.get('/updateEmployee', async (req, res) => {
     const {
         employee_id, department, experience, employee_status,first_name,last_name,current_band,gross_monthly_salary_or_fee_rs, title,date_of_joining,employee_type
     } = req.query;
-    console.log(req.query, "req")
 
     try {
         const employee = await db("employee_details").where({ employee_id }).first();
@@ -19,16 +18,32 @@ router.get('/updateEmployee', async (req, res) => {
             await db("employee_details").where({ employee_id }).update({ employee_id, department, experience, employee_status,first_name,last_name,current_band,gross_monthly_salary_or_fee_rs, title,date_of_joining,employee_type
             });
 
-            console.log(`Employee ${employee_id} updated.`);
             return res.status(200).json({ message: "Employee updated in database" });
         } else {
-            return res.status(404).json({ error: "Employee not found" });
+            await db("employee_details").insert({ employee_id, department, experience, employee_status, first_name, last_name, current_band, gross_monthly_salary_or_fee_rs, title, date_of_joining, employee_type });
+            return res.status(200).json({ message: "Employee created in database" });
         }
 
     } catch (error) {
         return res.status(400).json({ error: "Error updating employee", message: error.message });
     }
 });
+
+router.get('/createEmployee', async (req, res) => {
+    const {
+        employee_id, department, experience, employee_status, first_name, last_name, current_band, gross_monthly_salary_or_fee_rs, title, date_of_joining, employee_type
+    } = req.query;
+    try {
+        const employee = await db("employee_details").where({ employee_id }).first();
+        if (employee) {
+            return res.status(409).json({ error: "Employee already exists" });
+        }
+        await db("employee_details").insert({ employee_id, department, experience, employee_status, first_name, last_name, current_band, gross_monthly_salary_or_fee_rs, title, date_of_joining, employee_type });
+        
+    } catch (error) {
+        res.status(400).json({ error: "Error creating employee", message: error.message });
+    }
+})
 
 
 
