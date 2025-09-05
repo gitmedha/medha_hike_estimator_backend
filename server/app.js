6f8a29b3d70fd2b091304686df8de7b22aa02a30
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-09-05 10:26:25 +0530

    added the express session:

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const session = require('express-session');
+const pg = require('pg');
+const PgSession = require('connect-pg-simple')(session);
+
 
 
 //data base connection initialization
-require('./config/db');
+const knex = require('./config/db');
 
 //cors configuration
 
@@ -21,6 +25,30 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(express.json());
+app.set('trust proxy', 1);
+const sessionMiddleware = session({
+  store: new PgSession({
+    conObject: {
+      connectionString: process.env.DATABASE_URL, // if you use a single URL
+      // OR, if you want to use your knexfile config, pass user, host, database, password, port here
+    },
+    tableName: 'user_sessions', // defaults to "session"
+    createTableIfMissing: true  // auto-creates table if not exists
+  }),
+  secret: process.env.SESSION_SECRET || 'supersecretkey',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
+    httpOnly: true, // prevents JS access
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    sameSite: 'lax'
+  }
+});
+
+app.use(sessionMiddleware);
+
+
 
 
 const employeeRoutes = require('./routes/employeeRoutes');
@@ -39,6 +67,16 @@ app.use('/api/increments',incrementalRoutes);
 app.use('/api/bonuses',bonuses);
 app.use('/api/zoho',zohoRoutes);
 app.use('/webhook',webHooks);
+
+app.get('/api/test-session', (req, res) => {
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
+  res.json({ message: `You have viewed this ${req.session.views} times` });
+});
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
